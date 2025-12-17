1050f9febbf855474f60c4c997b97cc3ae222c22
bala-amypo <bala@amypo.in>
2025-12-17 09:16:17 +0000

    User commit

--- src/main/java/com/example/demo/newservice/NewfileServiceimpl.java
+++ src/main/java/com/example/demo/newservice/NewfileServiceimpl.java
@@ -1,5 +1,28 @@
 package com.example.demo.newservice.newserviceimpl;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.example.demo.newentity.NewfileEntity;
+import com.example.demo.newservice.NewfileService;
+@Service
+public class NewfileServiceImpl implements NewfileService {
 
-public class NewfileServiceImpl{
-    
+    @Override
+    public NewfileEntity savedata(NewfileEntity newfile) {
+    }
+
+    @Override
+    public NewfileEntity getidval(Long id) {
+    }
+
+    @Override
+    public List<NewfileEntity> getall() {
+    }
+
+    @Override
+    public NewfileEntity update(Long id, NewfileEntity newfile) {
+    }
+
+    @Override
+    public void delete(Long id) {
+    }
 }
